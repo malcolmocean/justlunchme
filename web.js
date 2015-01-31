653fa512021ed299403dbc85737a880ef8db2de0
Malcolm Ocean <malcolm.m.ocean@gmail.com>
2015-01-31 05:13:38 -0500

    switch hjs to mustache and add bootstrap

--- web.js
+++ web.js
@@ -1,93 +1,105 @@
- var express = require('express');
- var path = require('path');
- var favicon = require('serve-favicon');
- var logger = require('morgan');
- var cookieParser = require('cookie-parser');
- var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
- var routes = require('./routes/index');
- var users = require('./routes/users');
+var app = express();
 
- var app = express();
+// var db = require('./controllers/database.js');
 
- // var db = require('./controllers/database.js');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+app.set('layout', 'layout');
+// app.enable('view cache');
+app.engine('mustache', require('hogan-express'))
 
- // view engine setup
- app.set('views', path.join(__dirname, 'views'));
- app.set('view engine', 'hjs');
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
- // uncomment after placing your favicon in /public
- //app.use(favicon(__dirname + '/public/favicon.ico'));
- app.use(logger('dev'));
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({ extended: false }));
- app.use(cookieParser());
- app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res, next) {
+  console.log("req.url", req.url);
 
- app.get('/', function(req, res, next) {
-  res.render('index', { title: 'JustLunch.me' });
+  res.render('index', {
+    title: 'JustLunch.me',
+    list: [
+      {name: "noah", program: "nano"},
+      {name: "will", program: "tron"},
+      {name: "malcolm", program: "syde"}
+    ]
+  });
 });
 
- app.use('/users', users);
 
- // catch 404 and forward to error handler
- app.use(function(req, res, next) {
-     var err = new Error('Not Found');
-     err.status = 404;
-     next(err);
- });
+app.get('/anotherpage', function(req, res, next) {
+  res.render('other');
+});
 
- // error handlers
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
- // development error handler
- // will print stacktrace
- if (app.get('env') === 'development') {
-     app.use(function(err, req, res, next) {
-         res.status(err.status || 500);
-         res.render('error', {
-             message: err.message,
-             error: err
-         });
-     });
- }
+// error handlers
 
- // production error handler
- // no stacktraces leaked to user
- app.use(function(err, req, res, next) {
-     res.status(err.status || 500);
-     res.render('error', {
-         message: err.message,
-         error: {}
-     });
- });
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 var debug = require('debug')('justlunchme:server');
 var http = require('http');
 
 /**
- * Get port from environment and store in Express.
- */
+* Get port from environment and store in Express.
+*/
 
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
- * Create HTTP server.
- */
+* Create HTTP server.
+*/
 
 var server = http.createServer(app);
 
 /**
- * Listen on provided port, on all network interfaces.
- */
+* Listen on provided port, on all network interfaces.
+*/
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
 /**
- * Normalize a port into a number, string, or false.
- */
+* Normalize a port into a number, string, or false.
+*/
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
@@ -106,8 +118,8 @@ function normalizePort(val) {
 }
 
 /**
- * Event listener for HTTP server "error" event.
- */
+* Event listener for HTTP server "error" event.
+*/
 
 function onError(error) {
   if (error.syscall !== 'listen') {
@@ -115,32 +127,32 @@ function onError(error) {
   }
 
   var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port
+  ? 'Pipe ' + port
+  : 'Port ' + port
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
+// handle specific listen errors with friendly messages
+switch (error.code) {
+  case 'EACCES':
+    console.error(bind + ' requires elevated privileges');
+    process.exit(1);
+    break;
+  case 'EADDRINUSE':
+    console.error(bind + ' is already in use');
+    process.exit(1);
+    break;
+  default:
+    throw error;
   }
 }
 
 /**
- * Event listener for HTTP server "listening" event.
- */
+* Event listener for HTTP server "listening" event.
+*/
 
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
+  ? 'pipe ' + addr
+  : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
