12d4b57430fc751fc46b2c76d6e0c2e0864b3cd5
Malcolm Ocean <malcolm.m.ocean@gmail.com>
2015-01-31 03:03:39 -0500

    forget silly bin/www

--- controllers/database.js
+++ controllers/database.js
@@ -1,11 +1,15 @@
 var pg = require('pg');
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM your_table');
+pg.connect(process.env.DATABASE_URL || process.env.JUSTLUNCHME_PG_URL,  function(err, client) {
+  err && console.log("err", err);
+  console.log("client", client);
+  var query = client.query('CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100))');
+  query.on('end', function() { client.end(); });
+  // var query = client.query('SELECT * FROM your_table');
 
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
+  // query.on('row', function(row) {
+  //   console.log(JSON.stringify(row));
+  // });
 });
 
 module.exports = {};
