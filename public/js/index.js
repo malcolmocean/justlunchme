c7419d858f8efc3532066caeec9510e08937a762
Malcolm Ocean <malcolm.m.ocean@gmail.com>
2015-02-01 05:45:33 -0500

    syncy friends stuff

--- public/js/index.js
+++ public/js/index.js
@@ -8,18 +8,23 @@ var KEYS = {
   ENTER: 13
 }
 
-app.filter('listfilter', [function () {
+
+app.filter('searchfilter', [function () {
   return function(items, search) {
     if (!search || !search.text) {
       return items;
     }
-    var filtered = [];
     search.text = search.text.toLowerCase();
-    angular.forEach(items, function(item) {
-      if( item.email.toLowerCase().indexOf(search.text) >= 0 ) filtered.push(item);
+    var filtered = [];
+    angular.forEach(items, function(item, ix) {
+      if (item && item.searchString.indexOf(search.text) >= 0 ) {
+        // item.ix = ix;
+        filtered.push(item);
+      }
     });
     search.singleton = undefined;
     search.validemail = undefined;
+    search.filtered = filtered;
     search.size = filtered.length;
     if (search.size == 1) {
       search.singleton = filtered[0];
@@ -35,9 +40,9 @@ app.controller('UserPickerCtrl', function ($scope, $http) {
     $http.post('/add', who).success(callback);
   }
   $scope.lunchList = [];
-  $scope.addByClick = function (ix) {
-    if (ix > -1) {
-      $scope.lunchList.push($scope.friends.splice(ix, 1)[0]);
+  $scope.addByClick = function (friend) {
+    if (friend && friend.ix > -1) {
+      $scope.lunchList.push($scope.friends.splice(friend.ix, 1)[0]);
     } else if ($scope.search.validemail) {
       $scope.lunchList.push({email: $scope.search.text})
       $scope.search = {};
@@ -65,36 +70,81 @@ app.controller('UserPickerCtrl', function ($scope, $http) {
       }
     }
   }
-  $scope.friends = [
-    {
-      email: "Noah MacCallum <noahmacca@gmail.com>",
-    },
-    {
-      email: "Adam Euerby <aeuerby@gmail.com>",
-    },
-    {
-      email: "Alisa McClurg <amcclurg@outlook.com>",
-    },
-    {
-      email: "Amanda Comeau <amanda.comeau9@gmail.com>",
-    },
-    {
-      email: "Benjamin Carr <benjamin.f.carr@gmail.com>",
-    },
-    {
-      email: "Charlotte Clarke <accredo.et.amo@gmail.com>",
-    },
-    {
-      email: "Connor Turland <connorturland@gmail.com>",
-    },
-    {
-      email: "Emma Dines <emmadines@gmail.com>",
-    },
-    {
-      email: "Jean Robertson <jean@viewpointonchange.com>",
-    },
-    {
-      email: "Tanya Williams <friendsofthefloor@gmail.com>",
-    },
-  ]
+
+  var lunchMap = {};
+
+  listCallback = function (list) {
+    $scope.friends = [];
+    angular.forEach(list, function(item, ix) {
+      if (item.email && !/@fut.io/.test(item.email) && !lunchMap[item.email] &&
+        !/@(reply|groups).facebook.com/.test(item.email)) {
+        item.searchString = item.name.toLowerCase() + ' ' + item.email.toLowerCase()
+        item.ix = $scope.friends.length;
+        $scope.friends.push(item);
+      }
+    });
+    // $scope.friends = list;
+    console.log("$scope.friends[1]", $scope.friends[1]);
+    $scope.$apply();
+  }
+
+  var user = {
+    email: "malcolm.m.ocean@gmail.com"
+  }
+
+  $http.get('/'+user.email+'/lunchList').success(function (list) {
+    if ($scope.lunchList && $scope.lunchList.length) {
+      $scope.lunchList = $scope.lunchList.concat(list);
+    } else {
+      $scope.lunchList = list;
+    }
+    for (var i=0; i<$scope.lunchList.length; i++) {
+      lunchMap[$scope.lunchList[i].email] = true;
+    }
+  });
 });
+
+setTimeout(function () {
+  listCallback([
+    {
+      name: "Noah MacCallum",
+      email: "noahmacca@gmail.com",
+    },
+    {
+      name: "Adam Euerby",
+      email: "aeuerby@gmail.com",
+    },
+    {
+      name: "Alisa McClurg",
+      email: "amcclurg@outlook.com",
+    },
+    {
+      name: "Amanda Comeau",
+      email: "amanda.comeau9@gmail.com",
+    },
+    {
+      name: "Benjamin Carr",
+      email: "benjamin.f.carr@gmail.com",
+    },
+    {
+      name: "Charlotte Clarke",
+      email: "accredo.et.amo@gmail.com",
+    },
+    {
+      name: "Connor Turland",
+      email: "connorturland@gmail.com",
+    },
+    {
+      name: "Emma Dines",
+      email: "emmadines@gmail.com",
+    },
+    {
+      name: "Jean Robertson",
+      email: "jean@viewpointonchange.com",
+    },
+    {
+      name: "Tanya Williams",
+      email: "friendsofthefloor@gmail.com",
+    },
+  ]);
+}, 20);
